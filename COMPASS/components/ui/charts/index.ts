742f534684d1cb340d03a6f542e96eaabddb719d
Artur Twardowski <artek9321@gmail.com>
2026-05-14 11:57:09 +0200

    chore: remove misc dead components
    Komponenty bez referencji z app/ lub używane tylko przez wcześniej
    usunięte klastry admin/dashboard/profile:
    
    - components/common/: CollapsibleSection, PermissionGate, ProtectedPage
    - components/projects/: ProjectGapAnalysis (151), ProjectsMockup (stub)
    - components/shared/FavoriteProjectsSection (173)
    - components/support/NoGuardianFallback
    - components/theme/: ColorModeToggle, ThemeQuickPicker, ThemeSwitcher
      (używane tylko przez usunięty AdminProfileSection)
    - components/ui/charts/* (4 chart komponenty + barrel)
    - components/ui/loading-skeleton.tsx

--- COMPASS/components/ui/charts/index.ts
+++
@@ -1,4 +0,0 @@
-export { ChartCard } from './ChartCard'
-export { DistributionPieChart } from './DistributionPieChart'
-export { StatusBarChart } from './StatusBarChart'
-export { TrendLineChart } from './TrendLineChart'
